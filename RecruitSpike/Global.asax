43b2c434f161661d3677186496bde190acf50a38
dkorpics7 <dkorpics@roadrunner.com>
2017-06-05 18:07:00 -0400

    Initial Commit

---
+++ RecruitSpike/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RecruitSpike.MvcApplication" Language="C#" %>
---
+++ RecruitSpike/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RecruitSpike.Startup))]
+namespace RecruitSpike
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
